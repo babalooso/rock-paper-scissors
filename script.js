eed1103176ae328906286780c68b248b120bafc7
Beth Anne <157641202+babalooso@users.noreply.github.com>
2024-03-11 15:00:24 -0700

    i think i got getComputerChoice fx right after googling, lol. now i'm attempting getPlayerChoice which receives user input from prompt, then returns user input to lower case

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const choices = ["rock", "paper", "scissors"];
 
+function getComputerChoice() {
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function getPlayerChoice() {
+    let userInput = prompt("Choose your weapon: Rock, paper, or scissors?");
+    return userInput.toLowerCase();
+}
