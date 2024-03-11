605dc4fa798e64077c1552af9b4ae4aee7d48b0a
Beth Anne <157641202+babalooso@users.noreply.github.com>
2024-03-11 15:51:50 -0700

    yikes, trying this again by following codecademy video. console still doesn't like what i'm doing even though i put what they put EXACTLY

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-const choices = ["rock", "paper", "scissors"];
 
-function getComputerChoice() {
-    return choices[Math.floor(Math.random() * choices.length)];
+function getPlayerChoice = userInput => {
+    userInput = userInput.toLowerCase();
+    if (userInput === "rock" || userInput === "paper" || userInput === "scissors") {
+        return userInput   
+    } else {
+        console.log("Error. Please choose rock, paper, or scissors.")
+    }
 }
 
-function getPlayerChoice() {
-    let userInput = prompt("Choose your weapon: Rock, paper, or scissors?");
-    return userInput.toLowerCase();
-}
+//const choices = ["rock", "paper", "scissors"];
+
+//function getComputerChoice() {
+   // return choices[Math.floor(Math.random() * choices.length)];
+//}
+
+
+
+//const playerChoice = getPlayerChoice();
+//const computerChoice = getComputerChoice();
+
+
+//function playRound(playerChoice, computerChoice) {
+        //if (computerChoice === playerChoice) {
+        //return ("It's a tie!");
+    //} else if ((computerChoice === "rock" && playerChoice === "scissors") || (computerChoice === "scissors" && playerChoice==="papler")
+    //|| (computerChoice === "paper" && playerChoice === "rock")) {
+       // return ("You lose! You chose " + "${playerChoice}" + " but the computer chose " + "${computerChoice}")
+    //} else {
+      //  return ("You win! You chose " + "${playerChoice}" + " and the computer chose " + "${computerChoice}");
+    //}
+//}
