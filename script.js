6ffc21231a7191d6722744ef71e1eebb5b7c3f74
Beth Anne <157641202+babalooso@users.noreply.github.com>
2024-03-11 20:14:33 -0700

    followed codecademy tutorial, but theirs only handles a single round. now going to try to figure out 5x.

--- script.js
+++ script.js
@@ -1,12 +1,75 @@
 
 const getPlayerChoice = userInput => {
     userInput = userInput.toLowerCase();
-    if (userInput === "rock" || userInput === "paper" || userInput === "scissors") {
-        return userInput   
+    if (userInput === "rock" || userInput === "paper" || userInput === "scissors" || userInput === "bomb") {
+        return userInput;   
     } else {
         console.log("Error. Please choose rock, paper, or scissors.")
     }
 }
+
+
+const getComputerChoice = () => {
+    const randomNumber = (Math.floor(Math.random() * 3));
+    switch (randomNumber) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
+
+const determineWinner = (playerChoice, computerChoice) => {
+    if (playerChoice === computerChoice) {
+        return "It's a tie!";
+    }
+
+    if (playerChoice === "rock") {
+        if (computerChoice === "paper") {
+            return "Sorry, computer won.";
+        } else {
+            return "You win!";
+        }
+    }
+
+    if (playerChoice === "paper") {
+        if (computerChoice === "scissors") {
+            return "Sorry, computer won.";
+        } else {
+            return "You win!";
+        }
+    }
+
+    if (playerChoice === "scissors") {
+        if (computerChoice === "rock") {
+            return "Sorry, computer won.";
+
+        } else {
+            return "You win!";
+        };
+    }
+        
+    if (playerChoice === "bomb") {
+        return "You threw: bomb. You win automatically!";
+    }
+    }
+
+
+
+const playGame = () => {
+    const playerChoice = getPlayerChoice('bomb');
+    const computerChoice = getComputerChoice();
+    console.log(`You threw: ${playerChoice}`);
+    console.log(`The computer threw: ${computerChoice}`);
+
+    console.log(determineWinner(playerChoice, computerChoice));
+};
+    
+playGame();
+
+
 
 //const choices = ["rock", "paper", "scissors"];
 
